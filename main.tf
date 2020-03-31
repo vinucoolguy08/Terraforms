bdc98c3098415bb02259840a0faef5e74592fdef
vinucoolguy08 <vineshcool1990@gmail.com>
2020-03-31 20:54:28 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,9 @@
-resource "azurerm_resource_group" "test" {
-  name     = "terraform-resource-group"
+provider "azurerm" {
+  version = "=2.3.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "test1" {
+  name     = "terraform-resource-group1"
   location = "West Europe"
 }
