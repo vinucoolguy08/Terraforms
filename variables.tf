5d433a9b667d7fee778bb8e8cc59482a644f43aa
vinucoolguy08 <vineshcool1990@gmail.com>
2020-03-31 14:24:03 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "resource-group-name" {
-  default = "terraform-resource-group"
-  description = "The prefix used for all resources in this example"
-}
-
-variable "location" {
-  default = "West Europe"
-  description = "The Azure location where all resources in this example should be created"
-}
